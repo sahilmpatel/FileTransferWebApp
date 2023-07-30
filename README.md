f136b2e4b1463f931ce568c58da954a03ac56f65
sahil patel <sahilmpa8141@gmail.com>
2023-07-30 18:34:31 +0530

    WebApp-2.0
    1. Multiple Files upload with progress bar.
    2. Selected Files to be listed in 'Downloads' page
       so that can be downloaded.
    3. QR code implementation to access the app url for devices.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # FileTransferWebApp
 
 Simple Web-App for sharing files between devices implemented using Flask
+
+1. Multiple Files upload with progress bar.
+2. Selected Files to be listed in 'Downloads' page
+   so that can be downloaded.
+3. QR code implementation to access the app url for devices.
---
+++ app/static/js/fileupload.js
@@ -0,0 +1,37 @@
+ $(document).ready(function() {
+        $('[data-bs-toggle="tooltip"]').tooltip();
+        $("form").submit(function(event) {
+            event.preventDefault();
+            let formData = new FormData();
+            let files = $('#fileInput')[0].files;
+
+            $.each(files, function(i, file) {
+                formData.append('files[]', file);
+                let progressBar = '<div class="progress">';
+                progressBar += '<div id="progress_' + i + '" class="progress-bar" style="width: 0%;"></div>';
+                progressBar += '</div>';
+                progressBar += '<div id="progressStatus_' + i + '">0%</div>';
+                $('#progressContainer').append(progressBar);
+            });
+
+            // Single Axios API call for all files with progress tracking
+            axios.post('/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                },
+                onUploadProgress: function(progressEvent) {
+                    let percentComplete = (progressEvent.loaded / progressEvent.total) * 100;
+                    $("div[id^='progress_']").width(percentComplete + '%');
+                    $("div[id^='progressStatus_']").text(percentComplete.toFixed(2) + '%');
+                }
+            })
+            .then(response => {
+                alert(response.data.message);
+                $("#fileInput").val("");
+                $("#progressContainer").empty();
+            })
+            .catch(error => {
+                alert(error.response.data.error);
+            });
+        });
+ });
